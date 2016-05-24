cd6555cc143106fbcad7014310532fbf641c06f8
Jakub Mikos <jakub.mikos@sky.uk>
2016-05-24 11:31:37 +0100

    FAKE setup

---
+++ fpoker/build.fsx
@@ -0,0 +1,19 @@
+﻿#r "packages/FAKE/tools/FakeLib.dll"
+open Fake
+
+let buildDir = "./build"
+
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "BuildApp" (fun _ ->
+    !! "*.fsproj"
+        |> MSBuildRelease buildDir "Build"
+        |> Log "AppBuild-Output: "
+)
+
+"Clean"
+    ==> "BuildApp"
+
+RunTargetOrDefault "BuildApp"
